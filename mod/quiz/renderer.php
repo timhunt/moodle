82789be1ed075ac45040f0972a4d083e863d5cee
Tim Hunt <T.J.Hunt@open.ac.uk>
2021-06-23 10:55:28 +0100

    MDL-71998 login: buttons sending users to login should use GET not POST

--- mod/quiz/renderer.php
+++ mod/quiz/renderer.php
@@ -902,10 +902,10 @@ class mod_quiz_renderer extends plugin_renderer_base {
     public function view_page_guest($course, $quiz, $cm, $context, $messages) {
         $output = '';
         $output .= $this->view_information($quiz, $cm, $context, $messages);
-        $guestno = html_writer::tag('p', get_string('guestsno', 'quiz'));
-        $liketologin = html_writer::tag('p', get_string('liketologin'));
-        $referer = get_local_referer(false);
-        $output .= $this->confirm($guestno."\n\n".$liketologin."\n", get_login_url(), $referer);
+        $output .= $this->confirm(html_writer::tag('p', get_string('guestsno', 'quiz')) .
+                html_writer::tag('p', get_string('liketologin')),
+                new single_button(new moodle_url(get_login_url()), get_string('continue'), 'get', true),
+                new moodle_url(get_local_referer(false)));
         return $output;
     }
 
