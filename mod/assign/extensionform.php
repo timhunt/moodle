d06e62ec6796ae01fd8761f31a90e9ba8c94f742
Tim Hunt <T.J.Hunt@open.ac.uk>
2024-04-19 15:40:11 +0100

    MDL-81612 assign: show current value when editing a student's extension

--- mod/assign/extensionform.php
+++ mod/assign/extensionform.php
@@ -53,10 +53,20 @@ class mod_assign_extension_form extends moodleform {
         $this->instance = $instance;
 
         // Get the assignment class.
+        /** @var assign $assign */
         $assign = $params['assign'];
         $userlist = $params['userlist'];
         $usercount = 0;
         $usershtml = '';
+
+        [$usercondition, $params] = $DB->get_in_or_equal(array_slice($userlist, 0, 5));
+        array_unshift($params, $assign->get_instance()->id);
+        $userinfo = $DB->get_records_sql("
+                SELECT u.*, auf.extensionduedate
+                  FROM {user} u
+             LEFT JOIN {assign_user_flags} auf ON auf.userid = u.id AND auf.assignment = ?
+                 WHERE u.id $usercondition
+            ", $params);
 
         // TODO Does not support custom user profile fields (MDL-70456).
         $extrauserfields = \core_user\fields::get_identity_fields($assign->get_context(), false);
@@ -65,17 +75,25 @@ class mod_assign_extension_form extends moodleform {
                 $usershtml .= get_string('moreusers', 'assign', count($userlist) - 5);
                 break;
             }
-            $user = $DB->get_record('user', array('id' => $userid), '*', MUST_EXIST);
+            $user = $userinfo[$userid];
+            if ($user->extensionduedate) {
+                $currentextension = ' ' . get_string('userextensioncurrent', 'assign', userdate($user->extensionduedate));
+            } else {
+                $currentextension = ' ' . get_string('userextensionnone', 'assign');
+            }
 
-            $usershtml .= $assign->get_renderer()->render(new assign_user_summary($user,
-                                                                    $assign->get_course()->id,
-                                                                    has_capability('moodle/site:viewfullnames',
-                                                                    $assign->get_course_context()),
-                                                                    $assign->is_blind_marking(),
-                                                                    $assign->get_uniqueid_for_user($user->id),
-                                                                    $extrauserfields,
-                                                                    !$assign->is_active_user($userid)));
-                $usercount += 1;
+            $usershtml .= $assign->get_renderer()->render(
+                new assign_user_summary($user,
+                $assign->get_course()->id,
+                has_capability('moodle/site:viewfullnames',
+                $assign->get_course_context()),
+                $assign->is_blind_marking(),
+                $assign->get_uniqueid_for_user($user->id),
+                $extrauserfields,
+                !$assign->is_active_user($userid),
+                $currentextension),
+            );
+            $usercount += 1;
         }
 
         $userscount = count($userlist);
@@ -98,9 +116,18 @@ class mod_assign_extension_form extends moodleform {
             $mform->addElement('static', 'cutoffdate', get_string('cutoffdate', 'assign'), userdate($instance->cutoffdate));
             $finaldate = $instance->cutoffdate;
         }
+        // If we are only editing the extension for a single user, and that user
+        // already has an extension, start the form at that date.
+        $defaultdate = $finaldate;
+        if ($usercount === 1) {
+            $currentdate = reset($userinfo)->extensionduedate;
+            if ($currentdate) {
+                $defaultdate = $currentdate;
+            }
+        }
         $mform->addElement('date_time_selector', 'extensionduedate',
                            get_string('extensionduedate', 'assign'), array('optional'=>true));
-        $mform->setDefault('extensionduedate', $finaldate);
+        $mform->setDefault('extensionduedate', $defaultdate);
 
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_INT);
