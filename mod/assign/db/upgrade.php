f2753ad8bbc41dce52236b270f288f74a859e4af
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 12:17:28 +0100

    MDL-83985 mod_assign: change name to 1333 chars

--- mod/assign/db/upgrade.php
+++ mod/assign/db/upgrade.php
@@ -109,5 +109,19 @@ function xmldb_assign_upgrade($oldversion) {
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2024121801) {
+
+        // Changing precision of field name on table assign to (1333).
+
+        $table = new xmldb_table('assign');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field fullname.
+        $dbman->change_field_precision($table, $field);
+
+        // Assign savepoint reached.
+        upgrade_mod_savepoint(true, 2024121801, 'assign');
+    }
+
     return true;
 }
