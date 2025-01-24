946a1d01c2a4abe6242cc020f846690df4128945
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 14:03:12 +0100

    MDL-83985 mod_choice: change name to 1333 chars

--- mod/choice/db/upgrade.php
+++ mod/choice/db/upgrade.php
@@ -40,6 +40,9 @@
  */
 
 function xmldb_choice_upgrade($oldversion) {
+    global $DB;
+    $dbman = $DB->get_manager();
+
     // Automatically generated Moodle v4.2.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -51,6 +54,20 @@ function xmldb_choice_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024121801) {
+
+        // Changing precision of field name on table choice to (1333).
+
+        $table = new xmldb_table('choice');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field fullname.
+        $dbman->change_field_precision($table, $field);
+
+        // Choice savepoint reached.
+        upgrade_mod_savepoint(true, 2024121801, 'choice');
+    }
 
     return true;
 }
