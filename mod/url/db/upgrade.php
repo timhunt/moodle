dcb7f7fd67713929d72d0528734d124c2a17dbcd
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 16:16:14 +0100

    MDL-83985 mod_url: change name to 1333 chars

--- mod/url/db/upgrade.php
+++ mod/url/db/upgrade.php
@@ -43,6 +43,9 @@
  */
 
 function xmldb_url_upgrade($oldversion) {
+    global $DB;
+    $dbman = $DB->get_manager();
+
     // Automatically generated Moodle v4.2.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -64,6 +67,19 @@ function xmldb_url_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024121801) {
+
+        // Changing precision of field name on table url to (1333).
+        $table = new xmldb_table('url');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field fullname.
+        $dbman->change_field_precision($table, $field);
+
+        // URL savepoint reached.
+        upgrade_mod_savepoint(true, 2024121801, 'url');
+    }
 
     return true;
 }
